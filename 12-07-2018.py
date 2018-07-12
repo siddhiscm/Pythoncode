f24d34a6d48789395436a2de38d65a8baf247467
Divyasiddhi <divyashree.k@siddhiworld.com>
2018-07-12 12:12:50 +0530

    my second commit

---
+++ 12-07-2018.py
@@ -0,0 +1,11 @@
+def greatest(a,b,c):
+    if a>b and a>c:
+        print "a is greatest number"
+    elif b>c:
+        print "b is greatest number"
+    else:
+        print "C is greatest number"
+    return
+
+greatest(12,32,10)
+

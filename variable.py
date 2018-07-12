c85f8d3df7f39962555007d35e26b9ec03d1886c
bhuvaneswarisiddhi <bhuvaneswari.p@siddhiworld.com>
2018-07-12 11:56:45 +0530

    adding variable.py

---
+++ variable.py
@@ -0,0 +1,23 @@
+"""#--local variabe
+def my_function():
+	name= "bhuna"
+	print " my name is",name 
+my_function()
+#global variable
+a= 10
+b= 20
+def add(a,b):
+	return a+b
+print add(a,b)
+print a
+"""
+a= 10
+b= 20
+def my_fun(a):
+	a=20
+	a=10
+	print a
+my_fun(a)
+print a
+
+
